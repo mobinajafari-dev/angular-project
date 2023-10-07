1156a309e98c67d99a0c613b0e6210afe289fa8a
unknown <pc-mobina@mobina.Aris.co>
2023-10-07 09:54:22 +0330

    router debugged - lazy loading added - shared folder added

---
+++ src/app/modules/page-not-found/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `<h1>404 ERROR - PAGE NOT FOUND</h1>
+    <p>
+      Uh oh, looks like the page you are looking for has moved or no longer
+      exists.
+    </p>`,
+  styles: [],
+})
+export class PageNotFoundComponent {}
