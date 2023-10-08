0916d971ee8ca635af2eb6494fe94ddc99d1a552
mobina jafari <mobinajafari.dev@gmail.com>
2023-10-08 23:00:07 +0330

    footer v.2

--- src/app/modules/not-found/not-found.component.ts
+++ src/app/modules/not-found/not-found.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedModule } from '@shared';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.sass']
+  styleUrls: ['./not-found.component.sass'],
 })
 export class NotFoundComponent {
-
+  constructor(private router: Router) {}
+  onChangeRoute(): void {
+    this.router.navigate(['home']);
+  }
 }
