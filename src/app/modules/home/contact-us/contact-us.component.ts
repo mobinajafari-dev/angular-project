e19093790370da0289269e3ea8872eb7734ecba5
unknown <pc-mobina@mobina.Aris.co>
2023-10-17 15:08:59 +0330

    about us page added

--- src/app/modules/home/contact-us/contact-us.component.ts
+++ src/app/modules/home/contact-us/contact-us.component.ts
@@ -49,57 +49,9 @@ export class ContactUsComponent implements OnInit {
     this.phoneValidate();
     this.emailValidate();
     this.contentValidate();
+    const x = this.contactUsForm.valueChanges.subscribe();
+    console.log(x);
   }
-
-  // this.contactUsForm = new FormGroup({
-  //   userData: new FormGroup({
-  //     userFirstName: new FormControl(null, [Validators.required]),
-  //     userLastName: new FormControl(null, [Validators.required]),
-  //   }),
-  //   userContact: new FormGroup({
-  //     userEmail: new FormControl(null, [
-  //       Validators.required,
-  //       Validators.email,
-  //     ]),
-  //     userPhone: new FormControl(null, [Validators.required]),
-  //   }),
-  //   userComment: new FormGroup({
-  //     topic: new FormControl(null, [Validators.required]),
-  //     comment: new FormControl(null, [Validators.required]),
-  //   }),
-  // });
-
-  // this.contactUsForm.errors?.['required'];
-
-  // const firstNameControl = this.contactUsForm.get([
-  //   'userData',
-  //   'userFirstName',
-  // ]);
-
-  // firstNameControl?.valueChanges.subscribe((x) => {
-  //   this.validateFirstNameControl(firstNameControl as FormControl);
-  // });
-
-  // this.indexFormGroup = formBuilder.group({
-  //   firstparasm: new FormControl(),
-  //   firstNested: formBuilder.group({
-  //     secondNested: formBuilder.group({}),
-  //   }),
-  // });
-
-  // private validateFirstNameControl(firstNameControl: FormControl): void {
-  //   this.firstNameErrorMessage;
-  //   if (
-  //     (firstNameControl.errors && firstNameControl.touched) ||
-  //     firstNameControl.dirty
-  //   ) {
-  //     if (firstNameControl.errors?.['required']) {
-  //       this.firstNameErrorMessage = 'لطفا فیلد را پر کنید';
-  //     } else if (firstNameControl.errors?.['minLength']) {
-  //       this.firstNameErrorMessage = 'نام باید حداقل 3 کارکتر داشته اشد';
-  //     }
-  //   }
-  // }
 
   // function
 
@@ -230,3 +182,53 @@ export class ContactUsComponent implements OnInit {
     });
   }
 }
+
+// this.contactUsForm = new FormGroup({
+//   userData: new FormGroup({
+//     userFirstName: new FormControl(null, [Validators.required]),
+//     userLastName: new FormControl(null, [Validators.required]),
+//   }),
+//   userContact: new FormGroup({
+//     userEmail: new FormControl(null, [
+//       Validators.required,
+//       Validators.email,
+//     ]),
+//     userPhone: new FormControl(null, [Validators.required]),
+//   }),
+//   userComment: new FormGroup({
+//     topic: new FormControl(null, [Validators.required]),
+//     comment: new FormControl(null, [Validators.required]),
+//   }),
+// });
+
+// this.contactUsForm.errors?.['required'];
+
+// const firstNameControl = this.contactUsForm.get([
+//   'userData',
+//   'userFirstName',
+// ]);
+
+// firstNameControl?.valueChanges.subscribe((x) => {
+//   this.validateFirstNameControl(firstNameControl as FormControl);
+// });
+
+// this.indexFormGroup = formBuilder.group({
+//   firstparasm: new FormControl(),
+//   firstNested: formBuilder.group({
+//     secondNested: formBuilder.group({}),
+//   }),
+// });
+
+// private validateFirstNameControl(firstNameControl: FormControl): void {
+//   this.firstNameErrorMessage;
+//   if (
+//     (firstNameControl.errors && firstNameControl.touched) ||
+//     firstNameControl.dirty
+//   ) {
+//     if (firstNameControl.errors?.['required']) {
+//       this.firstNameErrorMessage = 'لطفا فیلد را پر کنید';
+//     } else if (firstNameControl.errors?.['minLength']) {
+//       this.firstNameErrorMessage = 'نام باید حداقل 3 کارکتر داشته اشد';
+//     }
+//   }
+// }
