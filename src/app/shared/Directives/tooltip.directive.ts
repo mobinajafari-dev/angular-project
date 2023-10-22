df039cf0801f4993371e4574f930879f7e614cbf
unknown <pc-mobina@mobina.Aris.co>
2023-10-22 16:56:54 +0330

    tooltip v.1.0.1

--- src/app/shared/Directives/tooltip.directive.ts
+++ src/app/shared/Directives/tooltip.directive.ts
@@ -1,121 +1,164 @@
-import {
-  Directive,
-  ElementRef,
-  HostListener,
-  Input,
-  Renderer2,
-} from '@angular/core';
+import { Directive, HostListener, Input, Renderer2 } from '@angular/core';
 import { IPosition } from '../Interfaces/position';
 
 @Directive({
   selector: '[appTooltip]',
 })
 export class TooltipDirective {
-  @Input('appTooltip') tooltipText!: string;
-  @Input() placement: 'right' | 'left' | 'bottom' | 'top' = 'bottom';
-  @Input() offset: number = 0;
-  hostElementPosition!: IPosition;
-  tooltipElementPosition!: IPosition;
-
-  private tooltipElement!: HTMLElement;
-  private hostElement!: HTMLElement;
-
   constructor(private renderer: Renderer2) {}
+
+  // getting data with input from the host element
+
+  // tooltip content
+
+  @Input('appTooltip') tooltipText!: string;
+
+  // tooltip placement
+
+  @Input({ required: true }) placement!: 'right' | 'left' | 'bottom' | 'top';
+
+  // tooltip offset by default is zero
+
+  @Input() offset: number = 0;
 
   @HostListener('mouseenter', ['$event.target']) onMouseEnter(event: any) {
     this.hostElement = event;
     this.hostElementPosition = event.getBoundingClientRect();
-
-    this.showTooltip();
+    if (!this.tooltipElement) {
+      this.createTooltip();
+    }
+    this.renderer.setStyle(this.tooltipElement, 'opacity', 1);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.hideTooltip();
+    this.renderer.setStyle(this.tooltipElement, 'opacity', 0);
   }
 
-  hideTooltip() {
-    // this.renderer.setStyle(this.tooltipElement, 'opacity', 0);
-  }
+  // host and tooltip element position
+
+  hostElementPosition!: IPosition;
+  tooltipElementPosition!: IPosition;
+
+  // host and tooltip element
+
+  private tooltipElement!: HTMLElement;
+  private hostElement!: HTMLElement;
 
   //parentNode
   //childNodes
 
-  showTooltip() {
-    console.log('showtooltip :', this.hostElement.childNodes);
-    this.renderer.appendChild(
-      this.hostElement.parentNode,
-      this.renderer.createElement('div')
-    );
-    // this.createTooltip();
-    // this.positionTooltip();
-    // this.renderer.setStyle(this.tooltipElement, 'opacity', 1);
-    // const parentElement = this.renderer.parentNode(this.elRef.nativeElement);
-    // this.renderer.appendChild(parentElement, this.tooltipElement);
-  }
-
   createTooltip() {
+    console.log('showtooltip :', this.hostElement.childNodes);
+
+    // creating div
+
     this.tooltipElement = this.renderer.createElement('div');
-    const text = this.renderer.createText(this.tooltipText);
-    this.renderer.appendChild(this.tooltipElement, text);
-    console.log(this.renderer);
-    // this.renderer.setStyle(this.tooltipElement, 'opacity', 0);
-    // this.renderer.setStyle(this.tooltipElement, 'background-color', '#999');
-    // this.renderer.setStyle(this.tooltipElement, 'position', 'absolute');
-    // this.renderer.setStyle(this.tooltipElement, 'z-index', '9999');
-    // this.renderer.setStyle(this.tooltipElement, 'transition', 'opacity 0.3s');
+
+    // creating the content of the div
+
+    const tooltipContent = this.renderer.createText(this.tooltipText);
+    this.renderer.appendChild(this.tooltipElement, tooltipContent);
+    this.renderer.appendChild(this.hostElement, this.tooltipElement);
+
+    // setting the style of host element to position relative
+
+    this.renderer.setStyle(this.hostElement, 'position', 'relative');
+
+    // setting the style of tooltip element
+
+    this.renderer.setStyle(this.tooltipElement, 'position', 'absolute');
+    this.renderer.setStyle(this.tooltipElement, 'z-index', '9999');
+    this.renderer.setStyle(this.tooltipElement, 'background-color', '#555');
+    this.renderer.setStyle(this.tooltipElement, 'color', '#f3f3f3');
+    this.renderer.setStyle(this.tooltipElement, 'padding', '5px');
+    this.renderer.setStyle(this.tooltipElement, 'border-radius', '5px');
+    this.renderer.setStyle(this.tooltipElement, 'cursor', 'default');
+
+    // adding the tooltip class to tooltip element in order to set after animation for making it's arrow
+
+    this.renderer.addClass(this.tooltipElement, 'tooltip');
+
+    this.positionTooltip();
   }
 
   positionTooltip() {
+    // getting the rect object for the tooltip element
+
     this.tooltipElementPosition = this.tooltipElement.getBoundingClientRect();
-    console.log(this.tooltipElementPosition);
-    let top = this.tooltipElementPosition.top;
-    let left = this.tooltipElementPosition.left;
+
+    // defining the left and top
+
+    let top, left;
+
     switch (this.placement) {
       case 'top':
-        top =
-          this.hostElementPosition.top -
-          this.tooltipElementPosition.height -
-          this.offset;
+        top = -(this.tooltipElementPosition.height + this.offset);
         left =
-          this.hostElementPosition.left +
           (this.hostElementPosition.width - this.tooltipElementPosition.width) /
-            2;
+          2;
+
+        this.renderer.addClass(this.tooltipElement, 'top');
+
         break;
+
       case 'bottom':
-        top = this.hostElementPosition.bottom + this.offset;
+        top = this.hostElementPosition.height + this.offset;
         left =
-          this.hostElementPosition.left +
           (this.hostElementPosition.width - this.tooltipElementPosition.width) /
-            2;
+          2;
+
+        this.renderer.addClass(this.tooltipElement, 'bottom');
+
         break;
-      case 'right':
-        top =
-          this.hostElementPosition.top +
-          (this.hostElementPosition.height -
-            this.tooltipElementPosition.height) /
-            2;
-        left = this.hostElementPosition.right + this.offset;
-        break;
+
       case 'left':
         top =
-          this.hostElementPosition.top +
           (this.hostElementPosition.height -
             this.tooltipElementPosition.height) /
-            2;
-        left =
-          this.hostElementPosition.left -
-          this.tooltipElementPosition.width -
-          this.offset;
+          2;
+        left = -(this.tooltipElementPosition.width + this.offset);
+
+        this.renderer.addClass(this.tooltipElement, 'left');
         break;
-      default:
-        throw new Error('invalid placement value' + this.placement);
+
+      case 'right':
+        top =
+          (this.hostElementPosition.height -
+            this.tooltipElementPosition.height) /
+          2;
+        left = this.hostElementPosition.width + this.offset;
+
+        this.renderer.addClass(this.tooltipElement, 'right');
+        break;
     }
+
+    // setting the top and left to the tooltip element
+
     this.renderer.setStyle(this.tooltipElement, 'top', `${top}px`);
     this.renderer.setStyle(this.tooltipElement, 'left', `${left}px`);
-    console.log(this.tooltipElementPosition);
-    console.log(this.tooltipElement.style.top);
   }
 }
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 // first trial
 
@@ -213,4 +256,56 @@ export class TooltipDirective {
 //   this.renderer.appendChild(this.el.nativeElement, this.tooltipElement);
 //   this.renderer.addClass(this.tooltipElement, 'tooltip');
 //   this.renderer.addClass(this.tooltipElement, `tooltip-${this.placement}`);
+// }
+
+// third trial
+// positionTooltip() {
+//   this.tooltipElementPosition = this.tooltipElement.getBoundingClientRect();
+//   console.log(this.tooltipElementPosition);
+//   let top = this.tooltipElementPosition.top;
+//   let left = this.tooltipElementPosition.left;
+//   switch (this.placement) {
+//     case 'top':
+//       top =
+//         this.hostElementPosition.top -
+//         this.tooltipElementPosition.height -
+//         this.offset;
+//       left =
+//         this.hostElementPosition.left +
+//         (this.hostElementPosition.width - this.tooltipElementPosition.width) /
+//           2;
+//       break;
+//     case 'bottom':
+//       top = this.hostElementPosition.bottom + this.offset;
+//       left =
+//         this.hostElementPosition.left +
+//         (this.hostElementPosition.width - this.tooltipElementPosition.width) /
+//           2;
+//       break;
+//     case 'right':
+//       top =
+//         this.hostElementPosition.top +
+//         (this.hostElementPosition.height -
+//           this.tooltipElementPosition.height) /
+//           2;
+//       left = this.hostElementPosition.right + this.offset;
+//       break;
+//     case 'left':
+//       top =
+//         this.hostElementPosition.top +
+//         (this.hostElementPosition.height -
+//           this.tooltipElementPosition.height) /
+//           2;
+//       left =
+//         this.hostElementPosition.left -
+//         this.tooltipElementPosition.width -
+//         this.offset;
+//       break;
+//     default:
+//       throw new Error('invalid placement value' + this.placement);
+//   }
+//   this.renderer.setStyle(this.tooltipElement, 'top', `${top}px`);
+//   this.renderer.setStyle(this.tooltipElement, 'left', `${left}px`);
+//   console.log(this.tooltipElementPosition);
+//   console.log(this.tooltipElement.style.top);
 // }
